3d39242249bdaecc0a7fea89fd23319b241a0df1
Shany Baruch <baruchshany@gmail.com>
2025-12-06 15:49:59 +0200

    Change files name local

--- src/services/toy.service.js
+++ src/services/toy.service.js
@@ -1,9 +1,12 @@
 
+import axios from 'axios'
 import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
 import { userService } from './user.service.js'
+import { httpService } from './http.service.js'
 
 const STORAGE_KEY = 'toyDB'
+const BASE_URL = 'toy/'
 
 export const toyService = {
     query,
@@ -11,66 +14,38 @@ export const toyService = {
     save,
     remove,
     getEmptyToy,
-    getRandomToy,
     getDefaultFilter,
-    getFilterFromSearchParams
+    getRandomToy
 }
 
+
 function query(filterBy = {}) {
-    return storageService.query(STORAGE_KEY)
-        .then(toys => {
-            if (!filterBy.txt) filterBy.txt = ''
-            if (!filterBy.maxPrice) filterBy.maxPrice = Infinity
-            if (filterBy.labels.length) {
-                toys = toys.filter(toy =>
-                    toy.labels.some(label => filterBy.labels.includes(label))
-                )
-            }
-            //sort
-            if (filterBy.sortBy) {
-                toys.sort((a, b) => {
-                    if (filterBy.sortBy === 'txt') return a.name.localeCompare(b.name)
-                    if (filterBy.sortBy === 'price') return a.price - b.price
-                    if (filterBy.sortBy === 'createdAt') return a.createdAt - b.createdAt
-                })
-            }
-            const regExp = new RegExp(filterBy.txt, 'i')
+    return httpService.get(BASE_URL, filterBy)
 
-            return toys.filter(toy =>
-                regExp.test(toy.name) &&
-                toy.price <= filterBy.maxPrice &&
-                (filterBy.inStock ? toy.inStock : true)
-
-            )
-        })
 }
 
 function getById(toyId) {
-    return storageService.get(STORAGE_KEY, toyId)
-}
+    return httpService.get(BASE_URL + toyId)
 
+}
 function remove(toyId) {
-    // return Promise.reject('Not now!')
-    return storageService.remove(STORAGE_KEY, toyId)
+    return httpService.delete(BASE_URL + toyId)
 }
-
 
 function save(toy) {
     if (toy._id) {
-        return storageService.put(STORAGE_KEY, toy)
+        return httpService.put(BASE_URL + toy._id, toy)
     } else {
-        // when switching to backend - remove the next line
-        toy.owner = userService.getLoggedinUser()
-        return storageService.post(STORAGE_KEY, toy)
+        return httpService.post(BASE_URL, toy)
     }
 }
+
 
 function getEmptyToy() {
     return {
         name: '',
         price: '',
-        labels: [],
-        inStock: true,
+        // speed: '',
     }
 }
 
@@ -78,38 +53,14 @@ function getRandomToy() {
     return {
         name: 'TOY-' + (Date.now() % 1000),
         price: utilService.getRandomIntInclusive(50, 300),
-        inStock: true
         // speed: utilService.getRandomIntInclusive(90, 200),
     }
 }
 
+
 function getDefaultFilter() {
-    return { txt: '', maxPrice: '', inStock: false, sortBy: '', labels: [] }
+    return { txt: '', maxPrice: '' }
 }
 
-function getFilterFromSearchParams(searchParams) {
-    const filterBy = {
-        txt: searchParams.get('txt') || '',
-        inStock: searchParams.get('inStock') === 'true',
-        price: +searchParams.get('maxPrice') || Infinity,
-        sort: searchParams.get('sortBy') || ''
-    }
 
-    return filterBy
-}
 
-// TEST DATA
-// storageService.post(STORAGE_KEY, {name: 'Subali Rahok 6', price: 980}).then(x => console.log(x))
-
-// const labels = ['On wheels', 'Box game', 'Art', 'Baby', 'Doll', 'Puzzle',
-//     'Outdoor', 'Battery Powered']
-// const toy = {
-//     _id: 't101',
-//     name: 'Talking Doll',
-//     imgUrl: 'hardcoded-url-for-now'
-//     ,
-//     price: 123,
-//     labels: ['Doll', 'Battery Powered', 'Baby'],
-//     createdAt: 1631031801011,
-//     inStock: true,
-// }
---
+++ src/services/toy.service.local.js
@@ -0,0 +1,115 @@
+
+import { storageService } from './async-storage.service.js'
+import { utilService } from './util.service.js'
+import { userService } from './user.service.js'
+
+const STORAGE_KEY = 'toyDB'
+
+export const toyService = {
+    query,
+    getById,
+    save,
+    remove,
+    getEmptyToy,
+    getRandomToy,
+    getDefaultFilter,
+    getFilterFromSearchParams
+}
+
+function query(filterBy = {}) {
+    return storageService.query(STORAGE_KEY)
+        .then(toys => {
+            if (!filterBy.txt) filterBy.txt = ''
+            if (!filterBy.maxPrice) filterBy.maxPrice = Infinity
+            if (filterBy.labels.length) {
+                toys = toys.filter(toy =>
+                    toy.labels.some(label => filterBy.labels.includes(label))
+                )
+            }
+            //sort
+            if (filterBy.sortBy) {
+                toys.sort((a, b) => {
+                    if (filterBy.sortBy === 'txt') return a.name.localeCompare(b.name)
+                    if (filterBy.sortBy === 'price') return a.price - b.price
+                    if (filterBy.sortBy === 'createdAt') return a.createdAt - b.createdAt
+                })
+            }
+            const regExp = new RegExp(filterBy.txt, 'i')
+
+            return toys.filter(toy =>
+                regExp.test(toy.name) &&
+                toy.price <= filterBy.maxPrice &&
+                (filterBy.inStock ? toy.inStock : true)
+
+            )
+        })
+}
+
+function getById(toyId) {
+    return storageService.get(STORAGE_KEY, toyId)
+}
+
+function remove(toyId) {
+    // return Promise.reject('Not now!')
+    return storageService.remove(STORAGE_KEY, toyId)
+}
+
+
+function save(toy) {
+    if (toy._id) {
+        return storageService.put(STORAGE_KEY, toy)
+    } else {
+        // when switching to backend - remove the next line
+        toy.owner = userService.getLoggedinUser()
+        return storageService.post(STORAGE_KEY, toy)
+    }
+}
+
+function getEmptyToy() {
+    return {
+        name: '',
+        price: '',
+        labels: [],
+        inStock: true,
+    }
+}
+
+function getRandomToy() {
+    return {
+        name: 'TOY-' + (Date.now() % 1000),
+        price: utilService.getRandomIntInclusive(50, 300),
+        inStock: true
+        // speed: utilService.getRandomIntInclusive(90, 200),
+    }
+}
+
+function getDefaultFilter() {
+    return { txt: '', maxPrice: '', inStock: false, sortBy: '', labels: [] }
+}
+
+function getFilterFromSearchParams(searchParams) {
+    const filterBy = {
+        txt: searchParams.get('txt') || '',
+        inStock: searchParams.get('inStock') === 'true',
+        price: +searchParams.get('maxPrice') || Infinity,
+        sort: searchParams.get('sortBy') || ''
+    }
+
+    return filterBy
+}
+
+// TEST DATA
+// storageService.post(STORAGE_KEY, {name: 'Subali Rahok 6', price: 980}).then(x => console.log(x))
+
+// const labels = ['On wheels', 'Box game', 'Art', 'Baby', 'Doll', 'Puzzle',
+//     'Outdoor', 'Battery Powered']
+// const toy = {
+//     _id: 't101',
+//     name: 'Talking Doll',
+//     imgUrl: 'hardcoded-url-for-now'
+//     ,
+//     price: 123,
+//     labels: ['Doll', 'Battery Powered', 'Baby'],
+//     createdAt: 1631031801011,
+//     inStock: true,
+// }

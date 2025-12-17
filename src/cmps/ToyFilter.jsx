960cc5c21d9ce69d45d8a978ffd3e07efef9df61
Shany Baruch <baruchshany@gmail.com>
2025-12-17 15:45:39 +0200

    Filter opens from the side

--- src/cmps/ToyFilter.jsx
+++ src/cmps/ToyFilter.jsx
@@ -9,22 +9,21 @@ export function ToyFilter({ filterBy, onSetFilter, sortBy, onSetSort, toyLabels 
     const [filterByToEdit, setFilterByToEdit] = useState({ ...filterBy })
     const debouncedOnSetFilter = useRef(utilService.debounce(onSetFilter, 300)).current
 
+    const [isOpen, setIsOpen] = useState(false)
+
     useEffect(() => {
         debouncedOnSetFilter(filterByToEdit)
     }, [filterByToEdit])
 
-
-    // function handleChange({ target }) {
-    //     let { value, name: field, type } = target
-    //     // console.log('value:', value)
-    //     if (type === 'select-multiple') {
-    //         value = [...target.selectedOptions].map(option => option.value)
-    //         console.log('value: ', value);
-
-    //     }
-    //     value = type === 'number' ? +value || '' : value
-    //     setFilterByToEdit(prevFilter => ({ ...prevFilter, [field]: value }))
-    // }
+    function onClearFilter() {
+        setFilterByToEdit({
+            name: '',
+            price: '',
+            inStock: '',
+            labels: [],
+            sortBy: ''
+        })
+    }
 
     function handleChange(ev) {
         let { value, name: field, type } = ev.target
@@ -47,114 +46,114 @@ export function ToyFilter({ filterBy, onSetFilter, sortBy, onSetSort, toyLabels 
     const { txt, inStock, labels } = filterByToEdit
 
     return (
-        <section className="toy-filter">
-            {/* <h2 className="title">Filter</h2> */}
+        <>
+            <div
+                className={`app-overlay ${isOpen ? 'open' : ''}`}
+                onClick={() => setIsOpen(false)}
+            ></div>
+            
+            <section className={`toy-filter ${isOpen ? 'open' : ''}`}>
 
-            <form onSubmit={onSubmitFilter} className="filtering flex">
-                <div className="filter-left-side">
-                    <input
-                        className="filter-by-name"
-                        type="search"
-                        name="txt"
-                        placeholder="Toy name"
-                        value={txt}
-                        onChange={handleChange}
-                    />
+                <button
+                    className="toggle-btn"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    {isOpen ? '✕' : 'Filter'}
+                </button>
 
-                    <input
-                        type="number"
-                        className="filter-by-price"
-                        name="maxPrice"
-                        placeholder="Max price"
-                        min={50}
-                        max={300}
-                        step={50}
-                        value={filterByToEdit.maxPrice || ''}
-                        onChange={handleChange}
-                    />
-
+                <div className="filter-header">
+                    <h3 className="title">Filter</h3>
+                    <button className="btn-clear" onClick={onClearFilter}>Clear</button>
                 </div>
-                <div className="filter-right-side">
-                    <select
-                        className="filter-by-stock"
-                        name="inStock"
-                        value={inStock || ''}
-                        onChange={handleChange}>
-                        <option value="">All</option>
-                        <option value="true">In Stock</option>
-                        <option value="false">Not in stock</option>
-                    </select>
 
-                    {/* {toyLabels &&
-                    <select
-                    multiple
-                    name="labels"
-                    value={labels || []}
-                    onChange={handleChange}
-                    >
-                    <option disabled value="">Labels</option>
-                    <>
-                    {toyLabels.map(label => (
-                        <option key={label} value={label}>
-                        {label}
-                        </option>
-                        ))}
-                        </>
+                <form onSubmit={onSubmitFilter} className="filtering flex">
+                    <div className="filter-left-side">
+                        <input
+                            className="filter-by-name"
+                            type="search"
+                            name="txt"
+                            placeholder="Toy name"
+                            value={txt}
+                            onChange={handleChange}
+                        />
+
+                        <input
+                            type="number"
+                            className="filter-by-price"
+                            name="maxPrice"
+                            placeholder="Max price"
+                            min={50}
+                            max={300}
+                            step={50}
+                            value={filterByToEdit.maxPrice || ''}
+                            onChange={handleChange}
+                        />
+
+                    </div>
+                    <div className="filter-right-side">
+                        <select
+                            className="filter-by-stock"
+                            name="inStock"
+                            value={inStock || ''}
+                            onChange={handleChange}>
+                            <option value="">All</option>
+                            <option value="true">In Stock</option>
+                            <option value="false">Not in stock</option>
                         </select>
-                        } */}
 
-                    {toyLabels && (
-                        <FormControl
-                            className="div-select"
-                            size="small"
-                        >
-                            <InputLabel id="labels-label">Labels</InputLabel>
-                            <Select
-                                labelId="labels-label"
-                                multiple
-                                name="labels"
-                                className="select-labels"
-                                value={labels || []}
-                                onChange={handleChange}
-                                input={<OutlinedInput label="Labels" />}
-
-                                MenuProps={{
-                                    PaperProps: {
-                                        className: 'toy-filter-menu',
-                                        style: {
-                                            maxHeight: 200,
-                                            width: 250,
-                                        },
-                                    }
-                                }}
-
-                                renderValue={(selected) => (
-                                    <Box sx={{
-                                        display: 'flex',
-                                        flexWrap: 'nowrap',
-                                        gap: 0.5,
-                                        overflow: 'hidden', 
-                                        textOverflow: 'ellipsis',
-                                        maxWidth: '100%'
-                                    }}>
-                                        {selected.map((value) => (
-                                            <Chip key={value} label={value} size="small" />
-                                        ))}
-                                    </Box>
-                                )}
+                        {toyLabels && (
+                            <FormControl
+                                className="div-select"
+                                size="small"
                             >
-                                {toyLabels.map((label) => (
-                                    <MenuItem key={label} value={label}>
-                                        {label}
-                                    </MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-                    )}
-                </div>
+                                <InputLabel id="labels-label">Labels</InputLabel>
+                                <Select
+                                    labelId="labels-label"
+                                    multiple
+                                    name="labels"
+                                    className="select-labels"
+                                    value={labels || []}
+                                    onChange={handleChange}
+                                    input={<OutlinedInput label="Labels" />}
 
-            </form>
-            <ToySort sortBy={sortBy} onSetSort={onSetSort} />
-        </section>
+                                    MenuProps={{
+                                        PaperProps: {
+                                            className: 'toy-filter-menu',
+                                            style: {
+                                                maxHeight: 200,
+                                                width: 250,
+                                            },
+                                        }
+                                    }}
+
+                                    renderValue={(selected) => (
+                                        <Box sx={{
+                                            display: 'flex',
+                                            flexWrap: 'nowrap',
+                                            gap: 0.5,
+                                            overflow: 'hidden',
+                                            textOverflow: 'ellipsis',
+                                            maxWidth: '100%'
+                                        }}>
+                                            {selected.map((value) => (
+                                                <Chip key={value} label={value} size="small" />
+                                            ))}
+                                        </Box>
+                                    )}
+                                >
+                                    {toyLabels.map((label) => (
+                                        <MenuItem key={label} value={label}>
+                                            {label}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        )}
+                    </div>
+
+                </form>
+                <ToySort sortBy={sortBy} onSetSort={onSetSort} />
+            </section>
+        </>
     )
 }

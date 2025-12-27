77ed0cf73b77bcf15b60a52ed9eaade4541be0ff
Shany Baruch <baruchshany@gmail.com>
2025-12-28 00:15:56 +0200

    Design review list

--- src/cmps/ReviewPreview.jsx
+++ src/cmps/ReviewPreview.jsx
@@ -4,8 +4,8 @@ export function ReviewPreview({ review }) {
     const { user, toy } = review
 
     return <article className="review-preview">
-        <p>Toy: <Link to={`/user/${toy._id}`}>{toy.name}</Link></p>
-        <p className="review-by">User: <Link to={`/user/${user._id}`}>{user.fullname}</Link></p>
+        <p><Link to={`/user/${toy._id}`}>{toy.name}</Link></p>
+        <p className="review-by"><Link to={`/user/${user._id}`}>{user.fullname}</Link></p>
         <p className="review-txt">{review.txt}</p>
     </article>
 }
